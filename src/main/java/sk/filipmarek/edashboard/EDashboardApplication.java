f527f825524d4b592cc4d107b4e0a2e11aeaa008
Filip Marek <mgrfilipmarek@gmail.com>
2024-04-05 21:14:07 +0200

    init commit

---
+++ src/main/java/sk/filipmarek/edashboard/EDashboardApplication.java
@@ -0,0 +1,13 @@
+package sk.filipmarek.edashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EDashboardApplication.class, args);
+    }
+
+}
